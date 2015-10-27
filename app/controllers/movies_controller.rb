018ebabca3ca53682868045ea9ab5c294c875359
dhheck <dheck@chicagobooth.edu>
2015-10-27 16:04:32 -0500

    DHH New Director Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,42 @@ class MoviesController < ApplicationController
 
   end
 
-  def create_movie
+  def create_row
+    @movie = Movie.new
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
 
+    @movie.save
+
+    render("show")
+  end
+
+  def edit_form
+    @movie = Movie.find(params[:id])
+  end
+
+  def update_row
+    @movie = Movie.find(params[:id])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
+
+    @movie.save
+
+    render("show")
+
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+
+    @movie.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,28 @@ Rails.application.routes.draw do
   #MOVIE Routes
 
   #Create a new Movie
-  get("/movies/new_form",        { :controller => "movies", :action => "new_form" })
-  get("/create_movie",  { :controller => "movies", :action => "create_movie" })
+  get("/movies/new_form",   { :controller => "movies", :action => "new_form" })
+  get("/create_movie",      { :controller => "movies", :action => "create_row" })
 
   #Read
   get("/movies",      { :controller => "movies", :action => "index" })
   get("/movies/:id",  { :controller => "movies", :action => "show" })
+
+  #Edit a Movie
+  get("/movies/:id/edit_form",   { :controller => "movies", :action => "edit_form" })
+  get('/update_movie/:id',       { :controller => 'movies', :action => 'update_row' })
+
+  #Delete a Movie
+  get('/delete_movie/:id',       { :controller => 'movies', :action => 'destroy' })
+
+  #DIRECTOR Routes
+
+  #Create a new Director
+  get("/directors/new_form",   { :controller => "directors", :action => "new_form" })
+  get("/create_director",      { :controller => "directors", :action => "create_row" })
+
+  #Read
+  get("/directors",      { :controller => "directors", :action => "index" })
+  get("/directors/:id",  { :controller => "directors", :action => "show" })
 
 end
